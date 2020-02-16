92be0c5e2c8cad2c7e3a3834b17c8410d621eca3
Daniel Paz <paz@evosocialsoft.com>
2020-02-16 00:57:43 +0000

    image views created

--- include/test.h
+++ include/test.h
@@ -84,6 +84,7 @@ class HelloTriangleApplication {
         std::vector<VkImage> swapChainImages;
         VkFormat swapChainImageFormat;
         VkExtent2D swapChainExtent;
+        std::vector<VkImageView> swapChainImageViews;
 
 		void initWindow() {
 			glfwInit();
@@ -98,8 +99,8 @@ class HelloTriangleApplication {
 			createSurface();
 			pickPhysicalDevice();
 			createLogicalDevice();
-            std::cout << "logical device created\n";
             createSwapChain();
+            createImageViews();
 		}
 
 		void mainLoop() {
@@ -513,6 +514,37 @@ class HelloTriangleApplication {
            swapChainImageFormat = surfaceFormat.format;
            swapChainExtent = extent;
         
+
+       }
+
+       void createImageViews() {
+           swapChainImageViews.resize(swapChainImages.size());
+           
+           for (size_t i = 0; i < swapChainImages.size(); i++) {
+
+               VkImageViewCreateInfo createInfo = {};
+               createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+               createInfo.image = swapChainImages[i];
+
+               createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+               createInfo.format = swapChainImageFormat;
+
+               createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+               createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+               createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+               createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+               
+               createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+               createInfo.subresourceRange.baseMipLevel = 0;
+               createInfo.subresourceRange.levelCount = 1;
+               createInfo.subresourceRange.baseArrayLayer = 0;
+               createInfo.subresourceRange.layerCount = 1;
+
+
+               if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                   throw std::runtime_error("failed to create image views!");
+               }
+            }
 
        }
 
